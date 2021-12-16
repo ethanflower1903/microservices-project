04d3b5df00a8b0bd7b4a50a271ed1e7088a2fed3
ethanflower1903 <flowerethan@aol.com>
2021-12-16 06:56:07 +0000

    added 3 UTs for customer-service

---
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/PetTest.java
@@ -0,0 +1,39 @@
+package org.springframework.samples.petclinic.customers.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+public class PetTest {
+    @Test
+    public void testGetName(){
+        //Arrange
+        Pet pet = new Pet();
+        //Act
+        pet.setName("Fluffy");
+        //Assert
+        assertEquals("Fluffy", pet.getName());
+    }
+    @Test
+    public void testGetOwner(){
+        //Arrange
+        Pet pet = new Pet();
+        Owner owner = new Owner();
+        owner.setFirstName("Call");
+        //Act
+        pet.setOwner(owner);
+        //Assert
+        assertEquals("Call", pet.getOwner().getFirstName());
+    }
+    @Test
+    public void testBirthDate(){
+        //Arrange
+        Pet pet = new Pet();
+        Date bd = new Date();
+        //Act
+        pet.setBirthDate(bd);
+        //Assert
+        assertEquals(bd,pet.getBirthDate());
+    }
+}
